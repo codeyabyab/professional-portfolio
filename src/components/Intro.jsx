7d5aa49dac55d7f790a250f204c88229e0b60e46
codeyabyab <kobyabyab@gmail.com>
2024-09-28 15:27:30 +0800

    28/9/2024 - Added Initial Intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,15 @@ import React from "react";
 function Intro() {
   return (
     <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-      Intro
+      <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">
+        Kobe Bryan Cayabyab
+      </h1>
+      <p className="text-base md:text-xl mb-3 font-medium">
+        Web Developer | Data Entry Specialist | Tech Professional
+      </p>
+      <p className="text-sm max-w-xl mb-6 font-bold">
+        This is my bio
+      </p>
     </div>
   );
 }
