8e9107330318792c9f855a7131942be1b0ff2066
codeyabyab <kobyabyab@gmail.com>
2024-09-28 15:36:15 +0800

    28/9/2024 - Added Portfolio Data

---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        title: "Company Based Inventory System",
+        imgUrl: "/assets/in-development.png",
+        stack: ["Laravel", "React", "Material-UI", "MySQL"],
+        link: "",
+    },
+    {
+        title: "Education-Based Notifier Web App",
+        imgUrl: "/assets/in-development.png",
+        stack: ["HTML", "CSS", "JavaScript", "JQuery", "PHP", "MySQL"],
+        link: "",
+    },
+    {
+        title: "Business Portfolio",
+        imgUrl: "/assets/in-development.png",
+        stack: [ "React", "Tailwind-CSS", "Vite"],
+        link: "",
+    },
+]
