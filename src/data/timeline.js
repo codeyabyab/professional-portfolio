d59f310e98c75207c269f39e31e52566a685c3b3
codeyabyab <kobyabyab@gmail.com>
2024-09-29 20:41:38 +0800

    29/9/2024 - Updated Timeline Data to have Type instead of Year

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,30 +1,30 @@
 export default [
     {
-        year: "Today",
+        type: "IT",
         title: "Data Annotator",
-        duration : "",
+        duration : "July 2024 - Today",
         details: "",
     },
     {
-        year: "Today",
+        type: "Office Ops",
         title: "Data Entry Specialist",
         duration : "July 2023 - December 2023",
         details: "",
     },
     {
-        year: "Today",
+        type: "Customer Support",
         title: "Online Chat Support",
         duration : "June 2023 - October 2023",
         details: "",
     },
     {
-        year: "Today",
+        type: "Office Ops",
         title: "Lead Generation Specialist",
         duration : "June 2023 - November 2023",
         details: "",
     },
     {
-        year: "Today",
+        type: "IT",
         title: "Web Development Intern",
         duration : "March 2023 - July 2023",
         details: "",
