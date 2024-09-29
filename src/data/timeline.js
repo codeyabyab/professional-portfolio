57e41e8835c709faa3e7e36cfa89221864dadf6c
codeyabyab <kobyabyab@gmail.com>
2024-09-29 08:00:07 +0800

    29/9/2024 - Added Timeline Data

---
+++ src/data/timeline.js
@@ -0,0 +1,33 @@
+export default [
+    {
+        year: "Today",
+        title: "Data Annotator",
+        duration : "",
+        details: "",
+    },
+    {
+        year: "Today",
+        title: "Data Entry Specialist",
+        duration : "July 2023 - December 2023",
+        details: "",
+    },
+    {
+        year: "Today",
+        title: "Online Chat Support",
+        duration : "June 2023 - October 2023",
+        details: "",
+    },
+    {
+        year: "Today",
+        title: "Lead Generation Specialist",
+        duration : "June 2023 - November 2023",
+        details: "",
+    },
+    {
+        year: "Today",
+        title: "Web Development Intern",
+        duration : "March 2023 - July 2023",
+        details: "",
+    },
+   
+]
