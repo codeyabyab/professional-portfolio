61b61f75dc443fac23a5545e579d14f102b776de
codeyabyab <kobyabyab@gmail.com>
2024-10-04 14:57:58 +0800

    4/10/2024 - Added Contribution Data

---
+++ src/data/contribution.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        title:"Thoth AI",
+        imgUrl: "/assets/contri/thoth-ai.png",
+    },
+    {
+        title:"NMS",
+        imgUrl: "/assets/contri/nms.png",
+    },
+    {
+        title:"LeadGen Crew",
+        imgUrl: "/assets/contri/leadgen-crew.png",
+    },
+    {
+        title:"Happy Wally",
+        imgUrl: "/assets/contri/happy-wally.png",
+    },
+    {
+        title:"TIRO AI",
+        imgUrl: "/assets/contri/tiro-ai.png",
+    },
+    {
+        title:"NOA Coaching",
+        imgUrl: "/assets/contri/noa-coach.png",
+    },
+   
+  
+
+]
