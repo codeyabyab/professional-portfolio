8d164c84978f6bf18ac423c752b15d049ac1e64e
codeyabyab <kobyabyab@gmail.com>
2024-11-22 18:20:54 +0800

    22/11/2024 - Added Certificate Data

---
+++ src/data/certificate.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        title: "The Complete 2024 Web Development Bootcamp",
+        imgUrl: "assets/in-development.png",
+        source: "Udemy"
+    },
+    {
+        title: "Python Fundamentals for Beginners",
+        imgUrl: "assets/in-development.png",
+        source: "Great Learning"
+    },
+    {
+        title: "React JS Tutorial",
+        imgUrl: "assets/in-development.png",
+        source: "Great Learning"
+    },
+    {
+        title: "Software Testing Tutorial",
+        imgUrl: "assets/in-development.png",
+        source: "Great Learning"
+    },
+    {
+        title: "UI/UX for Beginners",
+        imgUrl: "assets/in-development.png",
+        source: "Great Learning"
+    },
+]
