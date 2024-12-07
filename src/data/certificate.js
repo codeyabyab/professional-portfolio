41bbbc37d436dc87e0da30f05168ad67e81ea9db
codeyabyab <kobyabyab@gmail.com>
2024-12-07 20:56:30 +0800

    7/12/2024 - Updated Certificate Data to use New Images

--- src/data/certificate.js
+++ src/data/certificate.js
@@ -1,27 +1,27 @@
 export default [
     {
         title: "The Complete 2024 Web Development Bootcamp",
-        imgUrl: "assets/in-development.png",
+        imgUrl: "assets/certs/u-web-bootcamp.png",
         source: "Udemy"
     },
     {
         title: "Python Fundamentals for Beginners",
-        imgUrl: "assets/in-development.png",
+        imgUrl: "assets/certs/gl-python.png",
         source: "Great Learning"
     },
     {
         title: "React JS Tutorial",
-        imgUrl: "assets/in-development.png",
+        imgUrl: "assets/certs/gl-react.png",
         source: "Great Learning"
     },
     {
         title: "Software Testing Tutorial",
-        imgUrl: "assets/in-development.png",
+        imgUrl: "assets/certs/gl-software-testing.png",
         source: "Great Learning"
     },
     {
         title: "UI/UX for Beginners",
-        imgUrl: "assets/in-development.png",
+        imgUrl: "assets/certs/gl-ui-ux.png",
         source: "Great Learning"
     },
 ]
