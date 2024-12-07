17e0a59d293a19096864e963ecf58fc608468e17
codeyabyab <kobyabyab@gmail.com>
2024-12-07 21:23:35 +0800

    7/12/2024 - Updated Certificate to display Latest Certificates

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -9,7 +9,7 @@ function Certificate() {
       <Title>Certificates</Title>
       <div className="flex flex-col md:flex-row items-center justify-center">
         <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
-          {certificate.map((cert) => (
+          {certificate.slice(0, 5).map((cert) => (
             <CertificateItem 
             imgUrl={cert.imgUrl} 
             title={cert.title} 
